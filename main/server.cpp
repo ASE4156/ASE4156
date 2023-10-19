0743bc7feb4189497b1218738d8e82396032bfdf
Daniel Kim <kdanielive@gmail.com>
2023-10-18 23:32:39 -0400

    added the second endpoin t

--- main/server.cpp
+++ main/server.cpp
@@ -55,10 +55,42 @@ int main() {
         request.reply(status_codes::OK, response_data);
     });
 
+    // adds listener to localhost port	
+    http_listener listener2(U("http://localhost:8080/revise/user/input"));
+    
+    listener2.support(methods::GET, handle_request);
+    
+    // POST respond_to_text_conversation endpoint implementation
+    listener2.support(methods::POST, [](http_request request) {
+        // Parse JSON request
+        auto json_value = request.extract_json().get();
+
+        // Ensure the JSON value contains a "text" field.
+        if (!json_value.has_field(U("text")) || !json_value[U("text")].is_string()) {
+            request.reply(status_codes::BadRequest, U("Missing or invalid 'text' field in JSON request."));
+            return;
+        }
+
+        auto user_input = json_value[U("text")].as_string();
+        auto processed_output = revise_text_conversation(utility::conversions::to_utf8string(user_input));
+
+        // Create response JSON
+        json::value response_data;
+        response_data[U("response")] = json::value::string(utility::conversions::to_string_t(processed_output));
+
+        // Send reply
+        request.reply(status_codes::OK, response_data);
+    });
+
     try {
         listener
             .open()
             .then([&listener]() { std::wcout << U("Listening...") << std::endl; })
+            .wait();
+
+	listener2
+	    .open()
+	    .then([&listener]() { std::wcout << U("Listening...") << std::endl; })
             .wait();
 
         while (true);
