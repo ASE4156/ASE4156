3ffb2dd25307363c60713b5797c83f6ae33c17a6
Daniel Kim <kdanielive@gmail.com>
2023-11-30 11:41:54 -0500

    added the validate token endpoint

--- src/api-endpoints/Token.h
+++ src/api-endpoints/Token.h
@@ -9,6 +9,7 @@ public:
     void handleCreationRequest(web::http::http_request request);
     void handleDeletionRequest(web::http::http_request request);
     void handleGetRequest(web::http::http_request request);
+    void handleValidateRequest(web::http::http_request request);
 };
 
 #endif // TOKEN_H 
--- src/main.cpp
+++ src/main.cpp
@@ -49,6 +49,11 @@ int main() {
         token_endpoint.handleGetRequest(request);
     });
 
+    http_listener token_validate_listener(U("http://localhost:8080/token/validate"));
+    token_validate_listener.support(methods::GET, [&token_endpoint](http_request request) {
+        token_endpoint.handleValidateRequest(request);
+    });
+
     // Listener for the conversation endpoints
     http_listener prompt_listener(U("http://localhost:8080/prompt"));
     prompt_listener.support(methods::POST, [&prompt](http_request request) {
@@ -74,6 +79,7 @@ int main() {
         token_creation_listener.open().wait();
         token_deletion_listener.open().wait();
         token_get_listener.open().wait();
+        token_validate_listener.open().wait();
         prompt_listener.open().wait();
         prompt_id_listener.open().wait();
 
