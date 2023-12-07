6406794425b1dc45323ee1b908b6c537d23329cb
weiz-me <weizhang45name@gmail.com>
2023-12-07 01:13:22 -0500

    report done

--- report_generate.sh
+++ report_generate.sh
@@ -1,4 +1,4 @@
 ./unit_test -s > reports/unit_test_report.txt
-./end_test -s > reports/EndtoEnd_test_report.txt
-
+./end_test -s > reports/endtoend_test_report.txt
 python3 styler_checker/run_cpplint.py . styler_checker/cpplint.py
+cppcheck --enable=all --std=c++17 . --output-file=reports/static_analysis_report.txt
